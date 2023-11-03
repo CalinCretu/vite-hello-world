1bdc493a154718b0e1ac0c3c9efb08c36ed31386
CalinCretu <calincretu1412@gmail.com>
2023-11-03 16:10:54 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<script>
+// import HelloWorld from './components/HelloWorld.vue'
+export default {
+  data() {
+    return {
+      message: 'La mia prima app con Vite!'
+    }
+  },
+  methods: {
+    sayHi() {
+      console.log('Hi!')
+    }
+  }
+}
+</script>
+
+<template>
+  <div @click="sayHi">
+    <h1 class="title">
+      {{ message }}
+    </h1>
+  </div>
+</template>
+
+<style scoped>
+.title {
+  color: yellowgreen;
+}
+</style>
